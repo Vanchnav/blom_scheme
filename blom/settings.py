a1f494ff18049df08bd7a3886e9eac3dbaa065af
Ivan Chudnov <ivanch_2000@mail.ru>
2022-03-14 21:53:34 +0300

    Fixing runtime

--- blom/settings.py
+++ blom/settings.py
@@ -11,8 +11,8 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
-import dj_database_url
-db_from_env = dj_database_url.config(conn_max_age=500)
+# import dj_database_url
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -22,13 +22,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = 'django-insecure-3x+#cic6db0r-fx=*)%333^!1(5i$b+yfn!_bs)@l2p!6&09ys'
-import os
-SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'django-insecure-3x+#cic6db0r-fx=*)%333^!1(5i$b+yfn!_bs)@l2p!6&09ys')
+SECRET_KEY = 'django-insecure-3x+#cic6db0r-fx=*)%333^!1(5i$b+yfn!_bs)@l2p!6&09ys'
+# import os
+# SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'django-insecure-3x+#cic6db0r-fx=*)%333^!1(5i$b+yfn!_bs)@l2p!6&09ys')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = True
-DEBUG = bool( os.environ.get('DJANGO_DEBUG', True) )
+DEBUG = True
+# DEBUG = bool( os.environ.get('DJANGO_DEBUG', True) )
 
 ALLOWED_HOSTS = []
 
@@ -47,7 +47,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
+    # 'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -88,6 +88,11 @@ DATABASES = {
 }
 
 
+# DATABASES = {
+#     'default': dj_database_url.config()
+# }
+# db_from_env = dj_database_url.config(conn_max_age=500)
+# DATABASES['default'].update(db_from_env)
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
 
