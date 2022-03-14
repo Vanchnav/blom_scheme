460d0a53e3b053e5263f4d9b09863fb12c4a6e7c
Ivan Chudnov <ivanch_2000@mail.ru>
2022-03-14 22:29:34 +0300

    One more

--- blom/settings.py
+++ blom/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 # import dj_database_url
 
 
@@ -23,7 +24,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-3x+#cic6db0r-fx=*)%333^!1(5i$b+yfn!_bs)@l2p!6&09ys'
-# import os
 # SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'django-insecure-3x+#cic6db0r-fx=*)%333^!1(5i$b+yfn!_bs)@l2p!6&09ys')
 
 # SECURITY WARNING: don't run with debug turned on in production!
@@ -128,7 +128,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
-# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATIC_URL = '/static/'
 
